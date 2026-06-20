303f59c2c85d2350ed3a9ae8c3269b3c80fc2a35
Tiago829 <stfoliveira298@gmail.com>
2026-06-20 23:09:37 +0100

    adicionar sistema de atualizações

--- utils/updater.py
+++ utils/updater.py
@@ -1,15 +1,30 @@
 import requests
+import os
+from utils.paths import get_pasta_app
 
-VERSAO_ATUAL = "0.1"
 URL_VERSAO = "https://raw.githubusercontent.com/tiago829/GestorPasswords/main/version.txt"
 URL_DOWNLOAD = "https://github.com/tiago829/GestorPasswords/releases"
 
+
+def get_versao_atual():
+    # Lê o version.txt que vem empacotado no .exe
+    import sys
+    if hasattr(sys, "_MEIPASS"):
+        caminho = os.path.join(sys._MEIPASS, "version.txt")
+    else:
+        caminho = "version.txt"
+
+    with open(caminho, "r") as f:
+        return f.read().strip()
+
+
 def verificar_atualizacao():
     try:
+        versao_atual = get_versao_atual()
         resposta = requests.get(URL_VERSAO, timeout=5)
         versao_nova = resposta.text.strip()
 
-        if versao_nova != VERSAO_ATUAL:
+        if versao_nova != versao_atual:
             return versao_nova
         return None
     except:
